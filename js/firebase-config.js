6e1c4f8c7dd9fbbe9af331a65163501cbfe4450e
seade95 <seade95@users.noreply.github.com>
2026-07-17 07:41:04 +0100

    Fix firebase-config.js compat SDK setup

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,9 @@
 var FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyClI13ldpl8UkwhQQvnB8OG0bKdk9YgdLE",
+  authDomain: "eduverse-508a7.firebaseapp.com",
+  projectId: "eduverse-508a7",
+  storageBucket: "eduverse-508a7.firebasestorage.app",
+  messagingSenderId: "500067704675",
+  appId: "1:500067704675:web:22e033dd77d39580dabd56",
+  measurementId: "G-M5DYNJHW44"
 };
